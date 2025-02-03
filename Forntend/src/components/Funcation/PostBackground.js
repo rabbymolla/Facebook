cf7352c1c1719782756c79b8ee70e7719d1032cc
M. SHOZAUL BARY <rabby16139@gmail.com>
2025-02-03 22:52:11 +0800

    ddd

--- Forntend/src/components/Funcation/PostBackground.js
+++ Forntend/src/components/Funcation/PostBackground.js
@@ -1,12 +1,23 @@
+import bg1 from "../../assets/postBackgrounds/1.jpg";
+import bg2 from "../../assets/postBackgrounds/2.jpg";
+import bg3 from "../../assets/postBackgrounds/3.jpg";
+import bg4 from "../../assets/postBackgrounds/4.jpg";
+import bg5 from "../../assets/postBackgrounds/5.jpg";
+import bg6 from "../../assets/postBackgrounds/6.jpg";
+import bg7 from "../../assets/postBackgrounds/7.jpg";
+import bg8 from "../../assets/postBackgrounds/8.jpg";
+import bg9 from "../../assets/postBackgrounds/9.jpg";
+import bg10 from "../../assets/postBackgrounds/10.jpg";
+
 export const PostBackground = [
-  "../../src/assets/postBackgrounds/1.jpg",
-  "../../src/assets/postBackgrounds/2.jpg",
-  "../../src/assets/postBackgrounds/3.jpg",
-  "../../src/assets/postBackgrounds/4.jpg",
-  "../../src/assets/postBackgrounds/5.jpg",
-  "../../src/assets/postBackgrounds/6.jpg",
-  "../../src/assets/postBackgrounds/7.jpg",
-  "../../src/assets/postBackgrounds/8.jpg",
-  "../../src/assets/postBackgrounds/9.jpg",
-  "../../src/assets/postBackgrounds/10.jpg",
+  bg1,
+  bg2,
+  bg3,
+  bg4,
+  bg5,
+  bg6,
+  bg7,
+  bg8,
+  bg9,
+  bg10,
 ];
